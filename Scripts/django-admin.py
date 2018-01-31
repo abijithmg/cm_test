fbb07f60e94d39c37d699936a15944c04d927952
Soumya <soumyaranjanb02@gmail.com>
2018-01-31 19:41:10 +0530

    New

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\documents\ecommerce_beta\ecommerce-6c24c03aaf84a4fb02d9cc855815ab0e581798ff\src\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Scripts/rst2man.py
@@ -0,0 +1,26 @@
+#!c:\users\user\documents\ecommerce_beta\ecommerce-6c24c03aaf84a4fb02d9cc855815ab0e581798ff\src\scripts\python.exe
+
+# Author: 
+# Contact: grubert@users.sf.net
+# Copyright: This module has been placed in the public domain.
+
+"""
+man.py
+======
+
+This module provides a simple command line interface that uses the
+man page writer to output from ReStructuredText source.
+"""
+
+import locale
+try:
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+from docutils.writers import manpage
+
+description = ("Generates plain unix manual documents.  " + default_description)
+
+publish_cmdline(writer=manpage.Writer(), description=description)
